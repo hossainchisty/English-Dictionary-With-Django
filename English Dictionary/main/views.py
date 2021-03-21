42f16c745b960ee4d88ef4e9dce936891efa8e0f
hossainchisty <hossain.chisty11@gmail.com>
2021-03-22 01:27:05 +0600

    ---Updated---

---
+++ English Dictionary/main/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from PyDictionary import PyDictionary
+from django.contrib import messages
+# Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def word(request):
+    try:
+        search = request.GET.get('search')
+        dictionary = PyDictionary()
+        meaning = dictionary.meaning(search)
+        synonyms = dictionary.synonym(search)
+        antonyms = dictionary.antonym(search)
+    except (RuntimeError, TypeError, NameError,KeyError):
+            messages.error(request, "No search results found. Please refine your word.")
+
+    finally:
+        context = {
+                'meaning': meaning['Noun'][0],
+                'synonyms': synonyms,
+                'antonyms': antonyms,
+                'search':search }
+    return render(request, 'word.html', context)
+
+
+
+
+   
